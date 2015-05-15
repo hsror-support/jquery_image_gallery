b8f5799a465a970277735d07032b778fd76d5c43
Dhara Joshi <dhara.joshi.hs@gmail.com>
2015-05-15 17:34:37 +0530

    first commit

---
+++ lib/jquery_image_gallery.rb
@@ -0,0 +1,7 @@
+require "jquery_image_gallery/version"
+
+module JqueryImageGallery
+  class JqueryImageGallery
+    puts 'initializing jQuery Image Gallery'
+  end
+end
---
+++ lib/jquery_image_gallery/version.rb
@@ -0,0 +1,3 @@
+module JqueryImageGallery
+  VERSION = "0.1.0"
+end
